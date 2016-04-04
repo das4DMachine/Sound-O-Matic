ef0aeafb1d4077ff733ae6c4da84ecfaa6617c2d
mflarup@gmail.com <mflarup@gmail.com>
2016-04-04 18:04:14 +0200

    Created a CollidingCharacter script and a prefab for CollisionSound

---
+++ Assets/CollidingCharacters.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollidingCharacters : MonoBehaviour {
+
+	public GameObject collidingSoundPrefab;
+	private Collider collider;
+	AudioSource audio;
+
+	// Use this for initialization
+	void Start () {
+		audio = GetComponent<AudioSource>();
+		collidingSoundPrefab = Resources.Load("CollisionSound") as GameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter(Collision collision) {
+	}
+
+	void OnTriggerEnter(Collider collider) {
+		Debug.Log (gameObject.name + " was triggered by " + collider.gameObject.name);
+		Instantiate(collidingSoundPrefab, transform.position, transform.rotation);
+		}
+}
